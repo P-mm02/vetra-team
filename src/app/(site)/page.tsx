5bcdda0580254a143f764c84527c4291d07ea8fd
Wee Home PC01 <poomtawee.mm02@gmail.com>
2026-01-17 11:47:12 +0700

    Trigger deploy

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,8 +3,6 @@ import styles from './page.module.css'
 import AboutHero from '@/app/(site)/about/AboutHero/AboutHero'
 import HomeHero from '@/app/(site)/HomeHero/HomeHero'
 
-const contactEmail = 'hello@example.com' // change later
-
 export default function Page() {
   return (
     <div className={styles.page}>
