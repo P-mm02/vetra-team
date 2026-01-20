e2a10d158994e813d6c16a4ee218344d4cb7eb4c
Wee Home PC01 <poomtawee.mm02@gmail.com>
2026-01-20 10:44:40 +0700

    Fix site map

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,4 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://vetra.asia/</loc></url>
   <url><loc>https://vetra.asia/about</loc></url>
@@ -6,3 +7,12 @@
   <url><loc>https://vetra.asia/services</loc></url>
   <url><loc>https://vetra.asia/contact</loc></url>
 </urlset>
+`
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=86400',
+    },
+  })
+}
