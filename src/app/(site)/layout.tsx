e083ae2cb5be3e85b59fc9bc2b4c110c36acc6f1
Wee Home PC01 <poomtawee.mm02@gmail.com>
2026-01-25 19:37:00 +0700

    Update GA4

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,15 +4,22 @@ import '@/styles/globals.css'
 
 import Nav from '@/components/nav/nav'
 import Footer from '@/components/footer/footer'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function SiteLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
+  const gaId = process.env.NEXT_PUBLIC_GA_ID
+
   return (
-    <>
-      <Nav />
-      {children}
-      <Footer />
-    </>
+    <html lang="en">
+      <body>
+        <Nav />
+        {children}
+        <Footer />
+
+        {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
+      </body>
+    </html>
   )
 }
